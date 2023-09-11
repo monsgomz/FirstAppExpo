e9cfcfcbd2a8a308b3de7fb0d3ce2b6cac7937f5
M Montserrat Gomez <montserrat.ga95@gmail.com>
2023-09-11 12:27:35 -0400

    tutorial project

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
 
 export default function App() {
-  return (
+
+let [selectedImage, setSelectedImage] = useState(null); //variable que va a cambiar de imagen
+  const PlaceholderImage = require('./assets/clouds.jpg');
+
+  const pickImageAsync = async() => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1
+    });
+  if(!result.canceled){
+    setSelectedImage(result.assets[0].uri);
+    console.log(result)
+  } else{
+    alert("you did not select any image");
+  }
+  }
+  
+
+  return ( 
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: '#fff' }}>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+      <ImageViewer PlaceholderImage={PlaceholderImage} selectedImage={selectedImage}/>
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme={'primary'} onPress={pickImageAsync} />
+        <Button label="Use this photo" />
+      </View>
+      {/* <Image source={{uri: 'https://images.unsplash.com/photo-1693742857885-3867bb972e16?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2100&q=80'}}></Image> */}
       <StatusBar style="auto" />
     </View>
   );
 }
-
+//reactnative.dev/docs/style
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 2,
+    backgroundColor: '#D4E2D4',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+    footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
---
+++ components/Button.js
@@ -0,0 +1,61 @@
+import { StyleSheet, View, Pressable, Text } from 'react-native';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+
+export default function Button({label, theme, onPress}){   
+        if (theme === "primary") {
+    return (
+      <View
+      style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#DBC4F0", borderRadius: 18 }]}
+      >
+        <Pressable
+          style={[styles.button, { backgroundColor: "#DBC4F0" }]}
+          onPress={onPress}
+        >
+          <FontAwesome
+            name="picture-o"
+            size={18}
+            color="#25292e"
+            style={styles.buttonIcon}
+          />
+          <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
+        </Pressable>
+    </View>
+    );
+  }
+    return (
+    <View style={styles.buttonContainer}>
+      <Pressable style={[styles.button, {backgroundColor: '#A1CCD1'}]} onPress={() => alert('You pressed a button.')}>
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+    }
+  const styles = StyleSheet.create({
+    buttonContainer: {
+    width: 320,
+    height: 60,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3,
+    },
+    button: {
+       width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    padding: 8,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    },
+    buttonLabel: {
+        color: '#ffff',
+        fontWeight: 'bold',
+        fontSize: 16
+    },
+    buttonIcon: {
+    paddingRight: 8,
+  },
+  })
